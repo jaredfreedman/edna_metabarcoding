b16cf980c384efeb53a2957571bb542999ab6c18
jaredfreedman <freedmaj@oregonstate.edu>
2021-12-16 11:01:07 -0800

    Create DADA2_ESV_Clustering.R

---
+++ Code/DADA2_ESV_Clustering.R
@@ -0,0 +1,13 @@
+## DADA2 ESV Clustering
+# Created 12/16/21
+
+library(dada2)
+
+rm(list = ls())
+graphics.off()
+
+setwd("Research/git/edna_metabarcoding/GC_VR_MiSeq_2021")
+
+# Question - Do I need to go through the entire DADA2 Pipeline? Or can i jump in at the ESV clustering step?
+
+

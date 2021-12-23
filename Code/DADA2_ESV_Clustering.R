46a0e710bba4d053d2bbab964137aacc02a6fe17
jaredfreedman <freedmaj@oregonstate.edu>
2021-12-23 13:53:21 -0500

    DADA2 and Length Dist.

--- Code/DADA2_ESV_Clustering.R
+++ Code/DADA2_ESV_Clustering.R
@@ -2,12 +2,14 @@
 # Created 12/16/21
 
 library(dada2)
+library(JAMP)
+
 
 rm(list = ls())
 graphics.off()
 
 
-# Load fastq files and sort into F and R files ----------------------------
+# Load fastq files and trim primer sequences ----------------------------
 
 #Set working directory
 wd <- "/Users/jaredfreedman/Research/git/edna_metabarcoding/GC_VR_MiSeq_2021/DADA2_Workflow"
@@ -17,9 +19,30 @@ setwd(wd)
 path <- "../demulti_fastq/_data" 
 list.files(path)
 
+
+
+
+# Sort fastq files into F and R -------------------------------------------
+
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
 fnFs <- sort(list.files(path, pattern="_R1.fastq", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern="_R2.fastq", full.names = TRUE))
 
+# Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
+# Only have to do it to fnFs, since it removes R1 or R2 designation
+sample.names <- sapply(strsplit(basename(fnFs), "_R1.fastq"), `[`, 1)
+sample.names
+
+
+# Inspect Read Quality ----------------------------------------------------
+
+plotQualityProfile(fnFs[1:2]) # visualize the quality profiles of F reads
+
+plotQualityProfile(fnRs[1:2]) # visualize the quality profiles of R reads
+
+# Both F and R reads are high quality (by the standards of the DADA2 manual). It is accepted that R reads are worse quality than F reads, and DADA2 accounts for this.
+
+
+# Filter and Trim ---------------------------------------------------------
 
 
--- Code/lenght_dist_plots.R
+++ Code/lenght_dist_plots.R
@@ -14,23 +14,23 @@ setwd("/Users/jaredfreedman/Research/git/edna_metabarcoding/GC_VR_MiSeq_2021")
 # Create new directory for files -----------------------------------------
 
 dir.create("_Length_distribution", 
-           path = "C_Cutadapt/_stats/_Length_distribution")
+           path = "DADA2_Workflow/_Length_distribution")
 
 
 # For loop that creates length dist plot for each file from C_cuta --------
 
 
 
-trimmed_files <- list.files(path = "C_Cutadapt/_Data", full.names = TRUE) # Create list of file names
+trimmed_files <- list.files(path = "demulti_fastq/_Data", full.names = TRUE) # Create list of file names
 nfile <- length(trimmed_files)
 ifile <- 1
 
 start_time <- Sys.time() # start timer to see how long the following for loop will take
 
-for(ifile in 1:nfile){
+#for(ifile in 1:nfile){
   
   file <- trimmed_files[ifile]   #create character string containing the file path and name
-  filename <- strsplit(trimmed_files[ifile], split="_PE_cut.fastq")[[1]]   #create character string that contains file name without _PE_cut or .fastq extention
+  filename <- strsplit(trimmed_files[ifile], split=".fastq")[[1]]   #create character string that contains file name without _PE_cut or .fastq extention
   filename <- strsplit(filename, split="/")[[1]][3]   #remove the file path from the filename (done by splitting string by /, and only taking the 3rd part)
   
   
@@ -40,7 +40,7 @@ for(ifile in 1:nfile){
   
   print(paste("Length distribution histogram created for ", filename, sep=""))
   
-}
+#}
 
 end_time <- Sys.time() # end timer once for loop is completed
 

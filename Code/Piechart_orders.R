c369388f04b9791ef7727a324f383c6d04a3d933
jaredfreedman <freedmaj@oregonstate.edu>
2022-01-08 10:31:31 -0800

    taxonomy analysis

--- Code/Piechart_orders.R
+++ Code/Piechart_orders.R
@@ -7,10 +7,11 @@ graphics.off()
 library(JAMP)
 library(dplyr)
 library(ggplot2)
+library(lessR)
 
 setwd("/Users/jaredfreedman/Research/git/edna_metabarcoding/GC_VR_MiSeq_2021")
 
-otu_taxa <- read.csv(file = "Cluster_otus_VR/5_OTU_table_0.01_taxonomy_subset.csv")
+otu_taxa <- read.csv(file = "../Taxonomy_Output/Boldigger_Grand_Canyon_output_30_11_21/GC_OTU_Taxa_0.01.csv")
 
 unique_orders <- table(otu_taxa$Order)
 
@@ -18,15 +19,17 @@ unique_orders_df <- as.data.frame(unique_orders)
 
 pie_labels <- paste0(unique_orders, " = ", round(100 * unique_orders/sum(unique_orders), 2), "%")
 
-pie(x=unique_orders, main = "Orders of 90 most commom \n COI fragments in Verde River Samples")
+PieChart(x=unique_orders, main = "Orders of 90 most commom \n COI fragments in Verde River Samples",)
+
+
 
 
 
 # Save this as a PDF ------------------------------------------------------
 
-pdf(file="./Visual_Output/")
+pdf(file="../Visual_Output/GC_orders.pdf")
 
-pie(x=unique_orders, main = "Orders of 90 most commom \n COI fragments in Verde River Samples")
+PieChart(x=unique_orders, main = "Orders of 90 most commom \n COI fragments in Grand Canyon Samples")
 
 dev.off()
 
---
+++ Code/taxonomy_analysis.R
@@ -0,0 +1,69 @@
+## Taxonomy Analysis
+# Created 1/8/22
+
+
+rm(list = ls())
+graphics.off()
+
+library(JAMP)
+library(dplyr)
+library(ggplot2)
+library(lessR)
+
+setwd("/Users/jaredfreedman/Research/git/edna_metabarcoding/GC_VR_MiSeq_2021")
+
+otu_taxa <- read.csv(file = "../Taxonomy_Output/Boldigger_Grand_Canyon_output_30_11_21/GC_OTU_Taxa_0.01.csv")
+
+unique_orders <- table(otu_taxa$Order)
+
+unique_orders_df <- as.data.frame(unique_orders)
+
+
+# Turn read count into presence/absence data ------------------------------
+otu_PA <- otu_taxa
+
+otu_matrix <- as.matrix(otu_PA[3:76]) #create matrix of OTU read counts
+otu_matrix[otu_matrix>0] <- 1 #assign value of 1 to any location with >0 reads
+otu_df <- as.data.frame(otu_matrix) #convert matrix back to df
+otu_PA <- otu_PA[-c(3:77)] #remove the read count data from otu_PA 
+
+otu_PA <- cbind(otu_PA, otu_df) #combine presence/absence data with taxonomy data
+
+
+
+columns <- colnames(otu_PA[3:76])
+numcol <- length(columns)
+i<-1
+
+
+
+#for(i in 1:74){
+  currentcol <- columns[i]
+  
+  otu_PA[currentcol][currentcol>=1] <- TRUE
+  
+  
+  otu_PA$currentcol[otu_PA$currentcol>=1] <- TRUE
+  otu_PA$currentcol
+  i <- i+1
+  
+#}
+
+  
+
+
+
+# Unique Spp, Genus, Orders -----------------------------------------------
+
+species_num <- length(unique(otu_taxa$Species))
+genus_num <-length(unique(otu_taxa$Genus))
+family_num <- length(unique(otu_taxa$Family))
+order_num <- length(unique(otu_taxa$Order))
+
+species_num
+genus_num
+family_num
+order_num
+
+diptera_spp <- length(unique(otu_taxa$Species[otu_taxa$Order=="Diptera"]))
+diptera_spp
